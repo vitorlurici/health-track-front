e68d81226583593bc6fbaae5abbfc8fbaad5edd8
vitorlosing1 <vitorluricii@hotmail.com>
2025-05-14 00:02:04 -0300

    home-update

--- src/app/core/layout/components/footer/footer.component.ts
+++ src/app/core/layout/components/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LogoComponent } from '../../../../shared/components/logo.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LogoComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
