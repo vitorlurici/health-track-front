1cb1d9d5d115a7ef40e546bf997f66ed5d83b634
Hédon Victor <120107222+hedonvictor@users.noreply.github.com>
2025-06-01 18:48:46 +0000

    added dialog form

--- src/app/features/dashboard/dashboard-page/dashboard-page.component.ts
+++ src/app/features/dashboard/dashboard-page/dashboard-page.component.ts
@@ -8,34 +8,27 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
+import { FooterComponent } from '../../../core/layout/components/footer/footer.component';
+import { PressaoDialogComponent } from '../pressao-diao-dialog/pressao-diao-dialog.component';
+import { LogoComponent } from '../../../shared/components/logo.component';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
+
+
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [CommonModule, MatToolbarModule, MatCardModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, MatToolbarModule, MatCardModule, MatIconModule, MatButtonModule, FooterComponent,MatDialogModule, PressaoDialogComponent, LogoComponent],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.scss'
 })
 export class DashboardPageComponent {
-  cards = [
-    {
-      title: 'Card 1',
-      image: 'https://via.placeholder.com/300x150',
-      description: 'Texto do card 1',
-    },
-    {
-      title: 'Card 2',
-      image: 'https://via.placeholder.com/300x150',
-      description: 'Texto do card 2',
-    },
-    {
-      title: 'Card 3',
-      image: 'https://via.placeholder.com/300x150',
-      description: 'Texto do card 3',
-    },
-    {
-      title: 'Card 4',
-      image: 'https://via.placeholder.com/300x150',
-      description: 'Texto do card 4',
-    },
-  ];
+    constructor(private dialog: MatDialog) {}
+    
+  abrirDialog(): void {
+    this.dialog.open(PressaoDialogComponent, {
+      width: '400px',
+    });
+  }
 }
