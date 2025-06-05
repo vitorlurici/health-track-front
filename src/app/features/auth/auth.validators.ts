dd2ffeebedcad1c158edf48d88ca793b69574e82
vitorlosing1 <vitorluricii@hotmail.com>
2025-06-05 00:50:45 -0300

    auth-update

--- src/app/features/auth/auth.validators.ts
+++ src/app/features/auth/auth.validators.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
-@Injectable({ providedIn: 'root' }) // Adicione este decorator
+@Injectable({ providedIn: 'root' })
 export class AuthValidators {
   static passwordStrength(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
@@ -39,6 +39,48 @@ export class AuthValidators {
         matchingControl.setErrors(null);
         return null;
       }
+    };
+  }
+
+  static cpfValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const cpf = control.value?.replace(/\D/g, '');
+
+      if (!cpf) return null;
+
+      if (cpf.length !== 11) {
+        return { invalidCpf: true };
+      }
+
+      let sum = 0;
+      let remainder;
+
+      for (let i = 1; i <= 9; i++) {
+        sum += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+      }
+      remainder = (sum * 10) % 11;
+
+      if (remainder === 10 || remainder === 11) {
+        remainder = 0;
+      }
+      if (remainder !== parseInt(cpf.substring(9, 10))) {
+        return { invalidCpf: true };
+      }
+
+      sum = 0;
+      for (let i = 1; i <= 10; i++) {
+        sum += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+      }
+      remainder = (sum * 10) % 11;
+
+      if (remainder === 10 || remainder === 11) {
+        remainder = 0;
+      }
+      if (remainder !== parseInt(cpf.substring(10, 11))) {
+        return { invalidCpf: true };
+      }
+
+      return null;
     };
   }
 }
